f63186232793838e43e15bc06fa22838a94026e2
Joshua Lee <me@jolly.sg>
2016-11-16 14:35:53 +0800

    added susi folder

---
+++ susi/susi.bat
@@ -0,0 +1,1 @@
+java -Xmx4g -cp weka.jar;soot-trunk.jar;soot-infoflow.jar;soot-infoflow-android.jar;susi.jar de.ecspride.sourcesinkfinder.SourceSinkFinder android.jar permissionMethodWithLabel.pscout out.pscout
